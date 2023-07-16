18bbc160e278aac362100e80965a5dc9e1fe3f71
mamapisouza <Pi204326@>
2023-07-16 15:08:39 -0300

    melhoria

--- src/main/java/Handler.java
+++ src/main/java/Handler.java
@@ -1,41 +1,58 @@
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
-import com.amazonaws.services.simpleemail.model.*;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
 
 import java.util.ArrayList;
+
+import static com.amazonaws.regions.Regions.*;
 
 
 public class Handler implements RequestHandler<SNSEvent, ArrayList<Object>> {
 
-    static final String HTMLBODY = "<h1>Amazon SES test (AWS SDK for Java)</h1>"
-            + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
-            + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>"
-            + "AWS SDK for Java</a>";
+    final String SUBJECT = "Bem vindo ao Seu imóvel aqui!";
 
-    static final String TEXTBODY = "This email was sent through Amazon SES "
-            + "using the AWS SDK for Java.";
-
-    static final String SUBJECT = "Amazon SES test (AWS SDK for Java)";
+    final String HTMLBODY = "<h1>É uma honra ter você aqui</h1>"
+            + "<p>Acesso sua conta clicando <a href='https://seuimovelaqui.vercel.app/'> aqui </a>";
 
     @Override
     public ArrayList<Object> handleRequest(SNSEvent event, Context context) {
         String destinatario = event.getRecords().get(0).getSNS().getMessage();
         try {
-            AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
-            SendEmailRequest request = new SendEmailRequest().withDestination(new Destination()
-                    .withToAddresses(destinatario)).withMessage(new Message().withBody(new Body()
-                    .withHtml(new Content().withCharset("UTF-8").withData(HTMLBODY)).withText(new Content()
-                            .withCharset("UTF-8").withData(TEXTBODY))).withSubject(new Content()
-                    .withCharset("UTF-8").withData(SUBJECT))).withSource("matheus.2015jf@hotmail.com");
-            SendEmailResult sendEmailResult = client.sendEmail(request);
-            context.getLogger().log(sendEmailResult.toString());
+            AmazonSimpleEmailService client = createClient();
+            SendEmailRequest request = createRequest(destinatario);
+            client.sendEmail(request);
         } catch (Exception ex) {
             context.getLogger().log(ex.getMessage());
         }
         return null;
+    }
+
+    AmazonSimpleEmailService createClient() {
+        return AmazonSimpleEmailServiceClientBuilder.standard().withRegion(US_EAST_1).build();
+    }
+
+    SendEmailRequest createRequest(String destination) {
+        return new SendEmailRequest().withDestination(createDestination(destination))
+                .withMessage(createMessage()).withSource("matheus.2015jf@hotmail.com");
+    }
+
+    Destination createDestination(String destination) {
+        return new Destination().withToAddresses(destination);
+    }
+
+    Message createMessage() {
+        return new Message().withBody(new Body().withHtml(createContent(HTMLBODY)))
+                .withSubject(createContent(SUBJECT));
+    }
+
+    Content createContent(String content) {
+        return new Content().withCharset("UTF-8").withData(content);
     }
 }
