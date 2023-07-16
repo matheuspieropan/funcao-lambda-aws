16a0031437a44f14310a9fe46f29527204a52a86
mamapisouza <Pi204326@>
2023-07-16 17:34:36 -0300

    melhoria template

--- src/main/java/Handler.java
+++ src/main/java/Handler.java
@@ -11,15 +11,86 @@ import com.amazonaws.services.simpleemail.model.SendEmailRequest;
 
 import java.util.ArrayList;
 
-import static com.amazonaws.regions.Regions.*;
+import static com.amazonaws.regions.Regions.US_EAST_1;
 
 
 public class Handler implements RequestHandler<SNSEvent, ArrayList<Object>> {
 
     final String SUBJECT = "Bem vindo ao Seu imóvel aqui!";
 
-    final String HTMLBODY = "<h1>É uma honra ter você aqui</h1>"
-            + "<p>Acesso sua conta clicando <a href='https://seuimovelaqui.vercel.app/'> aqui </a>";
+    final String HTMLBODY = "<html>" +
+            "" +
+            "<head>" +
+            "    <style>" +
+            "        .conteudo-primeira-div {" +
+            "            color: white;" +
+            "            text-align: center;" +
+            "            width: 100%;" +
+            "        }" +
+            "" +
+            "        .blocos {" +
+            "            font-size: 16px;" +
+            "            margin-bottom: 20px;" +
+            "        }" +
+            "" +
+            "        .image {" +
+            "            width: 240px;" +
+            "        }" +
+            "" +
+            "        a {" +
+            "            color: white !important;" +
+            "            text-decoration: none;" +
+            "        }" +
+            "" +
+            "        .botao {" +
+            "            background-color: rgb(24, 33, 155);" +
+            "            border-radius: 28px;" +
+            "            display: inline-block;" +
+            "            color: #ffffff;" +
+            "            font-family: Arial;" +
+            "            font-size: 17px;" +
+            "            padding: 10px 20px;" +
+            "            border: none;" +
+            "        }" +
+            "    </style>" +
+            "</head>" +
+            "" +
+            "<body" +
+            "    style=\"background-color: white !important;color: black;width: 90%;font-family: 'Nunito Sans', sans-serif; margin: 0 auto;\">" +
+            "    <div style=\"background-color: rgb(24, 33, 155) !important;display: flex;\">" +
+            "        <div class=\"conteudo-primeira-div\">" +
+            "            <h1>SEJA BEM VINDO!</h1>" +
+            "        </div>" +
+            "    </div>" +
+            "" +
+            "    <div style=\"padding: 15px\">" +
+            "        <h2>Prezado usuário,</h2>" +
+            "" +
+            "        <div class=\"blocos\">" +
+            "            Seja bem-vindo ao nosso sistema de anúncio de imóveis! Estamos empolgados por você ter se juntado à nossa" +
+            "            comunidade de proprietários e locatários em busca do lugar perfeito para morar." +
+            "        </div>" +
+            "" +
+            "        <div class=\"blocos\">" +
+            "            Nosso sistema oferece uma plataforma abrangente para encontrar, anunciar e explorar uma ampla variedade de" +
+            "            imóveis disponíveis. Seja você um proprietário que deseja anunciar seu imóvel ou um locatário em busca de" +
+            "            uma nova casa, estamos aqui para ajudar." +
+            "        </div>" +
+            "" +
+            "        <div class=\"blocos\">" +
+            "            <button class=\"botao\"><a href=\"https://seuimovelaqui.vercel.app/\" target=\"_blank\">Acessar" +
+            "                    plataforma</a></button>" +
+            "        </div>" +
+            "" +
+            "        <div class=\"blocos\">" +
+            "            <div style=\"text-align: center;\">" +
+            "                <img class=\"image\" src=\"https://seuimovelaqui.vercel.app/assets/foto/Logo.png\">" +
+            "            </div>" +
+            "        </div>" +
+            "    </div>" +
+            "</body>" +
+            "" +
+            "</html>";
 
     @Override
     public ArrayList<Object> handleRequest(SNSEvent event, Context context) {
